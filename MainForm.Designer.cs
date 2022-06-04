a78db2a695c09b551ea64f789677fb0385354c5e
ieb <1@wa.vg>
2022-06-04 17:48:16 +0800

    no more fody

---
+++ MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SteamLobbist
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(422, 186);
+            this.listView1.TabIndex = 0;
+            this.listView1.TileSize = new System.Drawing.Size(64, 64);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 230);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(446, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 204);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(236, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(338, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(254, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 252);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "CS:GO Lobbist by ieb";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net.WebSockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Steamworks;
+
+
+namespace SteamLobbist
+{
+    
+    public partial class MainForm : Form
+    {
+
+        List<string> steamIdList = new List<string>();
+        bool isJoining = false;
+        string targetSteamId = "";
+        ClientWebSocket webSocket = new ClientWebSocket();
+        int sequenceid = 0;
+        bool finalized = true;
+        public MainForm()
+        {
+
+            Environment.SetEnvironmentVariable("SteamAppId", "480");
+            Environment.SetEnvironmentVariable("SteamOverlayGameId", "480");
+            Environment.SetEnvironmentVariable("SteamGameId", "480");
+            SteamAPI.Init();
+            webSocket.Options.SetRequestHeader("Origin", "https://steamcommunity.com");
+            webSocket.ConnectAsync(new Uri("ws://127.0.0.1:27060/clientsocket/"), CancellationToken.None);
+            InitializeComponent();
+        }
+        void LoadFriends()
+        {
+            ImageList imageList = new ImageList();
+            imageList.ImageSize = new Size(48, 48);
+            imageList.ColorDepth = ColorDepth.Depth32Bit;
+
+            listView1.LargeImageList = imageList;
+            listView1.Items.Clear();
+            steamIdList.Clear();
+
+            var friendCount = SteamFriends.GetFriendCount(EFriendFlags.k_EFriendFlagImmediate);
+
+            for (int idx = 0; idx < friendCount; idx++)
+            {
+                var m_Friend = SteamFriends.GetFriendByIndex(idx, EFriendFlags.k_EFriendFlagImmediate);
+                string personaName = SteamFriends.GetFriendPersonaName(m_Friend);
+                EPersonaState personaState = SteamFriends.GetFriendPersonaState(m_Friend);
+                int avatarId = SteamFriends.GetLargeFriendAvatar(m_Friend);
+                SteamFriends.GetFriendGamePlayed(m_Friend, out FriendGameInfo_t gameInfo);
+                if (gameInfo.m_gameID.m_GameID == 730) {
+                    if (avatarId != -1)
+                    {
+                        SteamUtils.GetImageSize(avatarId, out uint avatarW, out uint avatarH);
+                        int bufferSize = (int)(avatarW * avatarH * 4);
+                        byte[] avatarARGB = new byte[bufferSize];
+                        SteamUtils.GetImageRGBA(avatarId, avatarARGB, bufferSize);
+
+                        for (int j = 0; j < bufferSize; j += 4)
+                        {
+                            avatarARGB[j + 0] ^= avatarARGB[j + 2];
+                            avatarARGB[j + 2] ^= avatarARGB[j + 0];
+                            avatarARGB[j + 0] ^= avatarARGB[j + 2];
+                        }
+
+                        Bitmap avatar = new Bitmap((int)avatarW, (int)avatarH, PixelFormat.Format32bppRgb);
+                        Rectangle BoundsRect = new Rectangle(0, 0, (int)avatarW, (int)avatarH);
+                        BitmapData bmpData = avatar.LockBits(BoundsRect, ImageLockMode.WriteOnly, avatar.PixelFormat);
+
+                        IntPtr ptr = bmpData.Scan0;
+                        Marshal.Copy(avatarARGB, 0, ptr, (int)avatarW * (int)avatarH * 4);
+                        avatar.UnlockBits(bmpData);
+                        imageList.Images.Add(m_Friend.ToString(), avatar);
+
+                        // var personaState = SteamFriends.GetFriendRichPresence(m_Friend, "status");
+                        Console.WriteLine(personaState);
+                        listView1.Items.Add(new ListViewItem(personaName, m_Friend.ToString()));
+                    }
+                    else
+                    {
+                        listView1.Items.Add(new ListViewItem(personaName));
+                    }
+                    steamIdList.Add(m_Friend.ToString());
+                }
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadFriends();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isJoining)
+            {
+                isJoining = false;
+                button1.Text = "Start";
+                progressBar.Style = ProgressBarStyle.Blocks;
+            }
+            else
+            {
+                isJoining = true;
+                button1.Text = "Stop";
+                progressBar.Style = ProgressBarStyle.Marquee;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                targetSteamId = steamIdList[listView1.SelectedIndices[0]];
+                toolStripStatusLabel.Text = "SteamID: " + targetSteamId;
+            }
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!isJoining || !finalized) return;
+            finalized = false;
+            sequenceid += 1;
+            string s = String.Format("{{\"friend_id\":\"{0}\",\"sequenceid\":{1},\"universe\":1,\"message\":\"ShowJoinGameDialog\"}}", targetSteamId, sequenceid);
+            byte[] b = Encoding.ASCII.GetBytes(s);
+            await webSocket.SendAsync(b, WebSocketMessageType.Text, true, CancellationToken.None);
+            var buffer = new byte[1024];
+            var resultStr = "";
+            WebSocketReceiveResult result;
+
+            do
+            {
+                result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+                resultStr += Encoding.ASCII.GetString(buffer, 0, result.Count).Replace("\n", "");
+            }
+            while (!result.EndOfMessage);
+
+            toolStripStatusLabel.Text = resultStr;
+            finalized = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadFriends();
+        }
+    }
+}
